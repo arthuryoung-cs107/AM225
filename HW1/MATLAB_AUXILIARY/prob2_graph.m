d1498cd25f390f9942b800c69b49629c3bea2884
Arthur Young <>
2021-02-11 00:12:01 -0500

    sending to ubuntu

--- HW1/MATLAB_AUXILIARY/prob2_graph.m
+++ HW1/MATLAB_AUXILIARY/prob2_graph.m
@@ -27,32 +27,18 @@ red = [red1; red2; red3; red4; red5];
 
 pink = [255 0 104 ; 243 0 112; 230 0 119 ; 216  0 125; 200 0 131; 183 0 136; 165 0 140; 146 0 143; 125 7 145; 102 20 146]*(255)^(-1);
 
-view_mat = [45, 45; 1, 0; 0, 90; 90, 0 ; 45, 0];
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-dims = dlmread('m_n.dat');
+dims = dlmread('../dat_dir/prob2_specs_threads8.dat');
 m = dims(1);
 n = dims(2);
 
-gradients_analytical = grad_eval_analytical(points);
-
 fig1 = figure('Name', 'Jet space, view 1', 'Renderer', 'painters', 'Position', fig_pos(1, :));
-view(view_mat(1, :));
 ylabel('x')
 xlabel('t')
 xlim([0 6])
 ylim([0 9])
 hold on
 
-sol_it = 1;
-
-figure(fig1.Number)
-scatter3(points(:, 1), points(:, 2), points(:, 3), ' o','LineWidth', 1, 'CData', points(:, 3))
-
-figure(fig2.Number)
-scatter3(points_noise(:, 1), points_noise(:, 2), points_noise(:, 3), ' o','LineWidth', 1, 'CData', points(:, 3))
-
 % un-noised G matrix
-G_true_visible = dlmread('G_true_visible.dat');
-G_true = (fread(fopen('./svdTRUE_dir/G_true.dat'), [n, m], 'float64=>float64'))';
+thread8_results = (fread(fopen('../dat_dir/prob2_results_threads8.dat'), [n, m], 'float64=>float64'))';

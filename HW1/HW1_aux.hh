e225768a3ac946ea5f8f1132903df13c03ec6a35
Arthur Young <arthuryoung@g.harvard.edu>
2021-02-10 19:33:54 -0500

    fixed

--- HW1/HW1_aux.hh
+++ HW1/HW1_aux.hh
@@ -3,9 +3,10 @@
 
 #include <cstdio>
 #include <stdint.h>
+#include "gsl/gsl_rng.h"
 
 double random_uni(double low, double high, uint64_t * carry);
-uint64_t casino_game(uint64_t * carry);
+uint64_t casino_game(gsl_rng * T);
 void cell_automaton(int m, int n, int number_threads);
 double uniform_random(double low, double high, uint64_t * carry);
 

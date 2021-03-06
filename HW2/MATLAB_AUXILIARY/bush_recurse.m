a60ba3fd01564f9c2a10e193f81ebf52f053ad73
Arthur Young <>
2021-03-05 21:13:58 -0500

    think I fixed it. Got up to 10 just fine

--- HW2/MATLAB_AUXILIARY/bush_recurse.m
+++ HW2/MATLAB_AUXILIARY/bush_recurse.m
@@ -1,17 +1,32 @@
 function mat_out = bush_recurse(mat_in, width_in)
   mat_out = mat_in;
   row_last = mat_in(size(mat_in, 1) , :);
-  i = width_in;
-  while i >= 2
+  i = 2;
+  while i <= width_in
     if (row_last(i-1) > (row_last(i) + 1)  )
       row_new = row_last;
       row_new(i) = row_new(i) + 1;
       row_new(i-1) = row_new(i-1) - 1;
       mat_out = [mat_out; row_new];
       row_last = row_new;
-      i = width_in;
+      i = 2;
+    elseif (i + 1 <= width_in)
+        if ((row_last(i-1) > row_last(i) ) && (row_last(i) > row_last(i+1) ) ) 
+            row_new = row_last;
+            row_new(i+1) = row_new(i+1) + 1;
+            row_new(i-1) = row_new(i-1) - 1;
+            mat_out = [mat_out; row_new];
+            row_last = row_new;
+            i = 2;
+        else
+            i = i + 1;
+        end
     else
-        i = i - 1;
+        i = i + 1;
     end
+        
   end
+  
+
+  
 end

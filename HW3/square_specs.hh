e618918358b5689d367672b517686714f9d32635
arthuryoung-cs107 <>
2021-03-27 00:55:26 -0400

    finished glue dependency matrix

--- HW3/square_specs.hh
+++ HW3/square_specs.hh
@@ -4,18 +4,26 @@
 class square_specs {
     public:
         const int n_sqrs, N;
-        int **coords, **grid_mat, **grid_vis;
-        int * ascii_num;
-        int * n_vec;
+        int n_glue;
+        int nn_glue;
+        int **coords, **grid_mat, **grid_vis,
+        ** vec_indices, // query vector index, get matrix index
+        ** glue_indices, // query glue index (0-623), get matrix index
+        *** sqr_indices, // query square number (0-20), get stack of vector order
+        ** mat_indices, // query matrix index, get vector index
+        *ascii_num, *n_vec, *nn_vec;
         double *** A_mats;
+        double *** A_glue;
 
         square_specs();
         ~square_specs();
 
         void grid_read();
-        void init();
+        void vec_assign();
         void grid_mat2vec(double ** mat, double * vec);
         void grid_vec2mat(double ** mat, double * vec);
+        void A_gen(double ** mat, int n);
+        void glue_assign();
     private:
 
 };

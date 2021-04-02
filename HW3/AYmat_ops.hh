d31b1d11a7235f55a293b83113229f6cc8679ced
Arthur Young <>
2021-04-02 00:28:21 -0400

    strass done

--- HW3/AYmat_ops.hh
+++ HW3/AYmat_ops.hh
@@ -6,5 +6,7 @@
 #include "AYmat.hh"
 
 void AYmat_mul(AYmat * A1, AYmat * A2, AYmat * A3);
+void AYmat_mul_Strass(AYmat * A1, AYmat * A2, AYmat * A3);
+void Strass_recurse(double ** A1, double ** A2, double ** A3, int N);
 
 #endif

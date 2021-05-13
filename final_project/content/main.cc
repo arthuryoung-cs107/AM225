d7562e2b53ca9eaccc2bb82bdd1f30a8be325220
arthuryoung-cs107 <>
2021-05-13 03:28:19 -0400

    threw in additional setup for corrupted runs

--- final_project/content/main.cc
+++ final_project/content/main.cc
@@ -99,6 +99,44 @@ void test3()
 
 }
 
+void test3_corrupt()
+{
+#pragma omp parallel for
+  for (int i = 500; i >= 0; i--)
+  {
+
+    double t0, t1, tend;
+    char specfile_X_noised[200];
+    memset(specfile_X_noised, 0, 199);
+    snprintf(specfile_X_noised, 200, "./aydat_dir_small_sim3/tem_corrupt%d", i );
+
+    char prefix1[200];
+    memset(prefix1, 0, 199);
+    snprintf(prefix1, 200, "./aydat_dir_small_sim3/sim_corrupt_%d", i);
+
+    AYmat * X_noised_main_T = aysml_read(specfile_X_noised);
+    AYmat * X_noised_main = X_noised_main_T->transpose_gen();
+
+    delete X_noised_main_T;
+
+    ADM * adm1 = new ADM(X_noised_main);
+    delete X_noised_main;
+
+    t0 = omp_get_wtime();
+    adm1->solve();
+    t1 = omp_get_wtime();
+
+    adm1->write_out(prefix1);
+
+    delete adm1;
+
+    tend = t1-t0;
+    printf("field %d done. Time elapsed: %f seconds\n", i, tend);
+
+  }
+
+}
+
 void test4()
 {
 #pragma omp parallel for
@@ -113,6 +151,44 @@ void test4()
     char prefix1[200];
     memset(prefix1, 0, 199);
     snprintf(prefix1, 200, "./aydat_dir_small_sim4/sim1_%d", i);
+
+    AYmat * X_noised_main_T = aysml_read(specfile_X_noised);
+    AYmat * X_noised_main = X_noised_main_T->transpose_gen();
+
+    delete X_noised_main_T;
+
+    ADM * adm1 = new ADM(X_noised_main);
+    delete X_noised_main;
+
+    t0 = omp_get_wtime();
+    adm1->solve();
+    t1 = omp_get_wtime();
+
+    adm1->write_out(prefix1);
+
+    delete adm1;
+
+    tend = t1-t0;
+    printf("field %d done. Time elapsed: %f seconds\n", i, tend);
+
+  }
+
+}
+
+void test4_corrupt()
+{
+#pragma omp parallel for
+  for (int i = 500; i >= 0; i--)
+  {
+
+    double t0, t1, tend;
+    char specfile_X_noised[200];
+    memset(specfile_X_noised, 0, 199);
+    snprintf(specfile_X_noised, 200, "./aydat_dir_small_sim4/tem_corrupt%d", i );
+
+    char prefix1[200];
+    memset(prefix1, 0, 199);
+    snprintf(prefix1, 200, "./aydat_dir_small_sim4/sim_corrupt_%d", i);
 
     AYmat * X_noised_main_T = aysml_read(specfile_X_noised);
     AYmat * X_noised_main = X_noised_main_T->transpose_gen();
@@ -175,6 +251,43 @@ void test5()
 
 }
 
+void test5_corrupt()
+{
+#pragma omp parallel for
+  for (int i = 500; i >= 0; i--)
+  {
+
+    double t0, t1, tend;
+    char specfile_X_noised[200];
+    memset(specfile_X_noised, 0, 199);
+    snprintf(specfile_X_noised, 200, "./aydat_dir_small_sim5/tem_corrupt%d", i );
+
+    char prefix1[200];
+    memset(prefix1, 0, 199);
+    snprintf(prefix1, 200, "./aydat_dir_small_sim5/sim_corrupt_%d", i);
+
+    AYmat * X_noised_main_T = aysml_read(specfile_X_noised);
+    AYmat * X_noised_main = X_noised_main_T->transpose_gen();
+
+    delete X_noised_main_T;
+
+    ADM * adm1 = new ADM(X_noised_main);
+    delete X_noised_main;
+
+    t0 = omp_get_wtime();
+    adm1->solve();
+    t1 = omp_get_wtime();
+
+    adm1->write_out(prefix1);
+
+    delete adm1;
+
+    tend = t1-t0;
+    printf("field %d done. Time elapsed: %f seconds\n", i, tend);
+
+  }
+
+}
 
 int main()
 {
@@ -183,6 +296,10 @@ int main()
   // test3();
   // test4();
   // test5();
+
+  // test3_corrupt();
+  // test4_corrupt();
+  // test5_corrupt();
 
   return 0;
 }
